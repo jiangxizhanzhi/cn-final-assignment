806af7efee4cf88409665da1df554508b6f4adce
jiangxizhanzhi <43139879+jiangxizhanzhi@users.noreply.github.com>
2020-08-02 00:14:04 +0800

    Update runboot.sh

--- runboot.sh
+++ runboot.sh
@@ -1,1 +1,1 @@
-java ${JAVA_OPS} -Duser.timezone=Asia/Shanghai -Djava.security.egd=file:/dev/./urandom -jar /app/cn.jar
+java ${JAVA_OPS} -Duser.timezone=Asia/Shanghai -Djava.security.egd=file:/dev/./urandom -jar /app/demo-0.0.1-SNAPSHOT.jar.jar
